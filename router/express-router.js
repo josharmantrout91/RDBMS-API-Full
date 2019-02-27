4861b14a9691d964c0995158d424511cb5c8cad6
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-27 18:18:32 -0500

    Successful seed add for tables cohorts and students. Successful GET to /api/cohorts and /api/students

--- router/express-router.js
+++ router/express-router.js
@@ -4,4 +4,26 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
+router.get("/cohorts", (req, res) => {
+  // get the roles from the database
+  db("cohorts")
+    .then(cohorts => {
+      res.status(200).json(cohorts);
+    })
+    .catch(error => {
+      res.status(500).json(cohorts);
+    });
+});
+
+router.get("/students", (req, res) => {
+  // get the roles from the database
+  db("students")
+    .then(students => {
+      res.status(200).json(students);
+    })
+    .catch(error => {
+      res.status(500).json(students);
+    });
+});
+
 module.exports = router;
