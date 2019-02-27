500826f16e69a2f090d3dbfa13e7833040000316
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-27 18:37:03 -0500

    Successful GET by ID to /api/cohorts and /api/students

--- router/express-router.js
+++ router/express-router.js
@@ -4,8 +4,27 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
+// endpoints here
+
+// ********* CREATE METHODS ********* //
+
+// POST a new cohort
+// router.post("/cohorts", (req, res) => {
+//     db("cohorts")
+//     .insert()
+//     .then(ids => {
+//         const [id] = ids;
+
+//         db("cohorts")
+//         .where({id})
+//     })
+// })
+
+// ********* READ METHODS ********* //
+
+// GET cohorts
 router.get("/cohorts", (req, res) => {
-  // get the roles from the database
+  // get the cohorts from the database
   db("cohorts")
     .then(cohorts => {
       res.status(200).json(cohorts);
@@ -15,14 +34,39 @@ router.get("/cohorts", (req, res) => {
     });
 });
 
+// GET a single cohort by id
+router.get("/cohorts/:id", (req, res) => {
+  db("cohorts")
+    .where({ id: req.params.id })
+    .then(cohort => {
+      res.status(200).json(cohort);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// GET students
 router.get("/students", (req, res) => {
-  // get the roles from the database
+  // get the students from the database
   db("students")
     .then(students => {
       res.status(200).json(students);
     })
     .catch(error => {
       res.status(500).json(students);
+    });
+});
+
+// GET a single student by id
+router.get("/students/:id", (req, res) => {
+  db("students")
+    .where({ id: req.params.id })
+    .then(student => {
+      res.status(200).json(student);
+    })
+    .catch(error => {
+      res.status(500).json(error);
     });
 });
 
